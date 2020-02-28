293429eb6c681ed8b9dc6df7d4678e200ceb3382
Andrea Picco <andrea.picco@unige.ch>
2020-02-28 16:22:19 +0100

    correct s declaration, remove .jar

--- particleTracker/ParticleTracker_.java
+++ particleTracker/ParticleTracker_.java
@@ -93,7 +93,7 @@ public class ParticleTracker_ implements PlugInFilter, Measurements, ActionListe
 	int max_coord = 0;			// max value of the loaded particle coordinates
 
 	/* radius scale for eccentricity */
-	float s = 1.5 ; 
+	double s = 1.5 ; 
 	
 	/** 
 	 * This method sets up the plugin filter for use.
